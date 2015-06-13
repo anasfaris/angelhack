e79d52a0c5ffb7b8aa6c7fe75331ed482d69a718
SirTroll <zcchew2011@gmail.com>
2015-06-14 03:05:16 +0800

    commented new code out

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 import os
-<<<<<<< HEAD
-import http.client
-=======
->>>>>>> 326a7363803dd1a2e29d4ca80fb7c09f816b5a7c
+
+#import http.client
+
 try:
   from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
   from SocketServer import TCPServer as Server
@@ -21,14 +20,13 @@ try:
   httpd.serve_forever()
 except KeyboardInterrupt:
   pass
-<<<<<<< HEAD
-params = urllib.parse.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
-headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-conn = http.client.HTTPConnection("bugs.python.org")
-conn.request("POST", "", params, headers)
-response = conn.getresponse()
-print(response.status, response.reason)
-=======
->>>>>>> 326a7363803dd1a2e29d4ca80fb7c09f816b5a7c
+
+# params = urllib.parse.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
+# headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+# conn = http.client.HTTPConnection("bugs.python.org")
+# conn.request("POST", "", params, headers)
+# response = conn.getresponse()
+# print(response.status, response.reason)
+
 httpd.server_close()
 
