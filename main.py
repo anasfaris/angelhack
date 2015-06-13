31d1bcea255a9b604693d45c7362b867d236ac2a
Anas Faris <anasfaris@yahoo.com>
2015-06-13 12:06:07 +0800

    Set up basic framework

---
+++ main.py
@@ -0,0 +1,26 @@
+#Gevent Server
+import sys
+if 'threading' in sys.modules:
+    del sys.modules['threading']
+from gevent import monkey; monkey.patch_all()
+
+#Bottle Framework
+from bottle import *
+import bottle
+import os
+
+#specifying the path for the files
+@route('/static/<filepath:path>')
+def server_static(filepath):
+	return static_file(filepath, root='.')
+
+@error(404)
+def error404(error):
+    return 'URL does not exist'
+
+@route("/")
+def main():
+	return template('index.html')
+
+run(reloader=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), server='gevent')
+
