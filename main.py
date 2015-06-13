3c2e7be03bcceef4779d5f24beece2a574329a99
imranariffin <imran.ariffin@mail.utoronto.ca>
2015-06-13 14:35:52 -0400

    get_title done, get_article show good promise

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ if 'threading' in sys.modules:
     del sys.modules['threading']
 # from gevent import monkey; monkey.patch_all()
 
-
 #Bottle Framework
 from bottle import *
 import bottle
 import os
+
+# get article
+from imran import get_page, get_article, test_urls
 
 #specifying the path for the files
 @route('/static/<filepath:path>')
@@ -23,5 +25,14 @@ def error404(error):
 def main():
 	return template('index.html')
 	
+# get articles
+# @route('/get_article/<>')
+
+url = test_urls[0]
+beautiful_page = get_page(url)
+print "url:"
+print url
+print "beautiful page:"
+print beautiful_page
 
 run(reloader=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
